51921d3e734f317c7c2cf0b332c49d039f61be74
ismail-el-barsi <luciferelbarsi@gmail.com>
2024-12-05 18:08:26 +0100

    first commit

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+  products: [
+    {
+      owner: '67487198ef8376e9c6a56380',
+      name: 'Chanel No. 5',
+      slug: 'chanel-no-5',
+      category: 'Parfum',
+      image: '/images/channel_n5.jpg',
+      price: 150,
+      stock: 8,
+      rating: 2.8,
+      description:
+        "Un parfum floral classique avec des notes d'aldéhydes, un grand classique intemporel.",
+    },
+    {
+      owner: '67487198ef8376e9c6a56380',
+      name: 'Dior Sauvage',
+      slug: 'dior-sauvage',
+      category: 'Parfum',
+      image: '/images/dior_sauvage.jpg',
+      price: 120,
+      stock: 12,
+      rating: 3.7,
+      description:
+        'Un parfum frais et audacieux avec des notes de bergamote, de poivre et de ambroxan.',
+    },
+    {
+      owner: '67487198ef8376e9c6a56380',
+      name: 'Tom Ford Black Orchid',
+      slug: 'tom-ford-black-orchid',
+      category: 'Parfum',
+      image: '/images/Tom_Ford_Black_Orchid.jpg',
+      price: 180,
+      stock: 5,
+      rating: 2.5,
+      description:
+        'Un parfum luxueux et sensuel, avec des notes de truffe noire, de ylang-ylang et de vanille.',
+    },
+
+    {
+      owner: '67498d48c92c256254086193',
+      name: 'Yves Saint Laurent Libre',
+      slug: 'ysl-libre',
+      category: 'Parfum',
+      image: '/images/ysl_libre.jpg',
+      price: 130,
+      stock: 10,
+      rating: 4.6,
+      description:
+        "Un parfum audacieux et floral avec des notes de lavande, de fleur d'oranger, et de vanille. Idéal pour les femmes modernes.",
+    },
+    {
+      owner: '67498d48c92c256254086193',
+      name: 'Gucci Bloom',
+      slug: 'gucci-bloom',
+      category: 'Parfum',
+      image: '/images/gucci_bloom.jpg',
+      price: 140,
+      stock: 6,
+      rating: 1.7,
+      description:
+        'Un parfum floral riche avec des notes de jasmin, de tubéreuse, et de racine de gingembre, créant une senteur fraîche et envoûtante.',
+    },
+    {
+      owner: '67498d48c92c256254086193',
+      name: 'Hermès Terre d’Hermès',
+      slug: 'hermes-terre',
+      category: 'Parfum',
+      image: '/images/hermes_terre.jpg',
+      price: 170,
+      stock: 7,
+      rating: 3.8,
+      description:
+        "Un parfum boisé et terreux, alliant des notes d'orange, de cèdre, de vétiver et de musc. Parfait pour un homme élégant et raffiné.",
+    },
+  ],
+};
+
+export default data;
